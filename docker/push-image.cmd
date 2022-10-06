57a6ebef27eeed57e45ed7bf433ba38ef88fa139
jakub.wszola <jakub.wszola@asseco.pl>
2022-10-06 13:04:30 +0200

    refactor push-image.cmd

--- docker/push-image.cmd
+++ docker/push-image.cmd
@@ -1,3 +1,1 @@
-CALL cd ../ & mvn clean package
-CALL cd ../ & docker build . --tag jakubwsz/crc-discovery-server -f docker/Dockerfile
-CALL cd ../ & docker push jakubwsz/crc-discovery-server:latest
+CALL cd ../ & mvn clean package & docker build . --tag jakubwsz/crc-discovery-server -f docker/Dockerfile & docker push jakubwsz/crc-discovery-server:latest
